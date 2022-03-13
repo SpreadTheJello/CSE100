fee11225e8b128303f7eb38da2800f3e0d4a9200
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2022-03-12 19:31:21 -0800

    Lab 07 - Complete

---
+++ Lab_07/afatali.cpp
@@ -0,0 +1,46 @@
+#include <limits>
+#include <iostream>
+
+using namespace std;
+
+void extendedBottomUpCutRod(int* p, int n){ // from textbook page 369
+    int* r = new int[n+1];
+    int* s = new int[n+1];
+    r[0] = 0;
+    int q = INT_MIN;
+    
+    for(int j = 1; j < n; j++){
+        q = INT_MIN;
+        for(int i = 1; i <= j; i++){
+            if(q < p[i] + r[j-i]){
+                q = p[i] + r[j-i];
+                s[j] = i;
+            }
+        }
+        r[j] = q;
+    }  
+
+    n = n - 1; // prints r and s values
+    cout << r[n] << endl;
+    while(n > 0){
+        cout << s[n] << " ";
+        n = n-s[n];
+    }
+    cout << "-1" << endl;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int* p = new int[n + 1];
+    p[0] = 0;
+
+    for(int i = 1; i < n + 1; i++){
+        cin >> p[i];
+    }
+
+    extendedBottomUpCutRod(p, n+1);
+
+    return 0;
+}
