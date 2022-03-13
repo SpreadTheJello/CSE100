72a43a61aaf6ca2518e10b0f6883de132d1d2870
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2022-03-12 18:47:09 -0800

    Lab 06 - Complete

--- Lab_06/afatali.cpp
+++ Lab_06/afatali.cpp
@@ -5,19 +5,61 @@ using namespace std;
 
 int m;
 
-void hashInsert(list<int> T[], int k){
-    int i = 0, j;
-    while(i != m){
-        j = k%m;
+int h(int k){ // hash function
+    return (k % m);
+}
+
+void hashInsert(list<int> T[], int k){ // hash insert
+    int i = 0, j = h(k);
+    if(m != 0){
         T[j].push_front(k);
-        i++;
-    }
-    if(i == m){
-        cout << "hash table overflow";    
     }
 }
 
-int main(){
+void hashPrint(list<int> T[]){ 
+    list<int> :: iterator iter; // from https://en.cppreference.com/w/cpp/container/list
+    for(int i = 0; i < m; i++){ // (used this in CSE30 with hash function before)
+        cout << i << ":"; // prints the "0: 1: 2:, etc."
+        for(iter = T[i].begin(); iter != T[i].end(); ++iter){ // print whats in that has according to the 0, 1, 2, etc.
+            cout << *iter << "->";
+        }
+        cout << ";" << endl;
+    }
+}
+
+void hashSearch(list<int> T[], int k){ 
+    list<int> :: iterator iter; // also learned from https://en.cppreference.com/w/cpp/container/list
+    bool found = false;
+    int j = h(k), i = 0;
+    for(iter = T[j].begin(); iter != T[j].end(); ++iter, i++){
+        if(*iter == k){
+            cout << k << ":FOUND_AT" << j << "," << i << ";" << endl;
+            found = true;
+        }
+    }
+    if(!found){
+        cout << k << ":NOT_FOUND;" << endl;
+    }
+}
+
+void hashDelete(list<int> T[], int k){
+    list<int> :: iterator iter; // same concept as before
+    bool removed = false;
+    int j = h(k), i = 0;
+    for(iter = T[j].begin(); iter != T[j].end(); ++iter, i++){
+        if(*iter == k){
+            T[i].erase(iter);
+            cout << k << ":DELETED;" << endl;
+            removed = true;
+            break;
+        }
+    }
+    if(!removed){
+        cout << k << ":DELETE_FAILED;" << endl;
+    }
+}
+
+int main(int argc, const char* argv[]){
     cin >> m;
     list<int>T[m];
     char command;
@@ -31,17 +73,18 @@ int main(){
         }
         if(command == 'd'){
             cin >> k;
-            //hashDelete(T, k);
+            hashDelete(T, k);
         }
         if(command == 's'){
             cin >> k;
-            //hashSearch(T, k);
+            hashSearch(T, k);
         }
         if(command == 'o'){
-            //hashPrint(T);
+            hashPrint(T);
         }
         if(command == 'e'){
             break;
         }
     }
+    return 0;
 }
