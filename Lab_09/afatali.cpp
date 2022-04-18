15ef0265afe3ed3dcc63b9812b86c495e8775ea5
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2022-04-18 10:32:37 -0700

    Lab 09 - Complete

--- Lab_09/afatali.cpp
+++ Lab_09/afatali.cpp
@@ -7,18 +7,18 @@ using namespace std;
 string HuffmanEncodings[0];
 
 struct Node {
-    Node* left;
-    Node* right;
+    Node* left; // basic left node for huffman tree
+    Node* right; // basic right node for huffman tree
     string letter;
     int frequency;
     int unique_id;
     
     Node(string letter, int frequency){
-        left = NULL;
+        left = NULL; 
         right = NULL;
         this->letter = letter;
         this->frequency = frequency;
-        this->unique_id = 0;
+        this->unique_id = 0; // set to 0 (no problem)
     }
     
     Node(string letter, int frequency, int unique_id){
@@ -26,12 +26,12 @@ struct Node {
         right = NULL;
         this->letter = letter;
         this->frequency = frequency;
-        this->unique_id = unique_id;
+        this->unique_id = unique_id; // dont set to 0 if theres a unique id
     }
 };
 
 struct compare {
-    bool operator() (Node* left, Node* right){
+    bool operator() (Node* left, Node* right){ // returns true or false if there is a unique id
         if(left->frequency == right->frequency){
             return (left->unique_id > right->unique_id);
         }
@@ -47,40 +47,40 @@ void print(Node* minHeapRoot, string encondings, string alphabet[], int size){
         return;
     }
     else{
-        if(minHeapRoot->letter.compare("$") != 0){ 
+        if(minHeapRoot->letter.compare("!") != 0){ // sets each letter with their bytes
             for (int i = 0; i < size; i++){
                 if(minHeapRoot->letter.compare(alphabet[i]) == 0){
                     HuffmanEncodings[i] = encondings;
                 }
             }
         }
-        if(minHeapRoot->letter.compare("$") == 0){
-            print(minHeapRoot->left, encondings + "0", alphabet, size);
-            print(minHeapRoot->right, encondings + "1", alphabet, size);
+        if(minHeapRoot->letter.compare("!") == 0){ // if its a summation node (parent)
+            print(minHeapRoot->left, encondings + "0", alphabet, size); // left traverse
+            print(minHeapRoot->right, encondings + "1", alphabet, size); // right traverse
         }
     }
 }
 
 void Huffman(int size, string A[], vector<int>& C) {
-    priority_queue<Node*, vector<Node*>, compare> HuffmanHeap;
-    Node* root = NULL;
+    priority_queue<Node*, vector<Node*>, compare> HuffmanHeap; // prirority queue is a list that is sorted greatest to least values
+    Node* root = NULL; // set our 3 nodes to NULL
     Node* left = NULL;
     Node* right = NULL;
     
     for (int i = 0; i < size; i++) {
-        Node* temp = new Node(A[i], C[i], i + 1);
+        Node* temp = new Node(A[i], C[i], i + 1); // set array of node containing letter, freq, unique_id
         HuffmanHeap.push(temp);
     }
-    
+
     while(HuffmanHeap.size() > 1) {
-        left = HuffmanHeap.top(); //extracts the min
+        left = HuffmanHeap.top(); // extracts the min
         HuffmanHeap.pop();
-        right = HuffmanHeap.top();
+        right = HuffmanHeap.top(); // extracts the 2nd min
         HuffmanHeap.pop();
         
-        Node* newParent = new Node("$", (left->frequency + right->frequency));
+        Node* newParent = new Node("!", (left->frequency + right->frequency)); // create summation node
         root = newParent;
-        if(left->frequency == right->frequency && left->unique_id > right->unique_id){
+        if(left->frequency == right->frequency && left->unique_id > right->unique_id){ // swaps the child nodes if left is greater than right
             root->left = right;
             root->right = left;
             root->unique_id = right->unique_id;
@@ -91,12 +91,12 @@ void Huffman(int size, string A[], vector<int>& C) {
             root->unique_id = left->unique_id;
         }
 
-        HuffmanHeap.push(root);
+        HuffmanHeap.push(root); // push in finalized summation root
     }
-    print(HuffmanHeap.top(), "", A, size);
+    print(HuffmanHeap.top(), "", A, size); 
     
     char j = 'A';
-    for (int i = 0; j <= 'F'; i++, j++) {
+    for (int i = 0; j <= 'F'; i++, j++) { // prints the "letter:byte"
         cout << j << ":" << HuffmanEncodings[i] << endl;
     }
 }
@@ -105,13 +105,13 @@ void Huffman(int size, string A[], vector<int>& C) {
 int main(int argc, char** argv) { 
     int size = 6;
     int value = 0;
-    vector<int> C;
-    string A[size];
+    vector<int> C; // input vector
+    string A[size]; // letters
     
     for (int i = 0; i < size; i++) {
         cin >> value;
-        C.push_back(value);
-        A[i] = "K" + to_string(i);
+        C.push_back(value); // adds input at end of C
+        A[i] = "K" + to_string(i); // set what each letter's order is
     }
     
     Huffman(size, A, C);
